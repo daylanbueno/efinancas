6df3a950b75d469f56c8f3bcf99b30a7b868115f
Dailan Bueno <daylansantos@gmail.com>
2020-06-06 21:21:30 -0300

    Valida campos salvar usuario

--- src/app/controller/UsuarioController.js
+++ src/app/controller/UsuarioController.js
@@ -1,7 +1,20 @@
+import * as yup from 'yup';
 import Usuario from '../models/Usuario';
+
+const validaSchema = yup.object().shape({
+  nome: yup.string().required('O campo nome é obrigatório '),
+  email: yup
+    .string()
+    .email('O email é invalido')
+    .required('O campo email é obrigatório'),
+  senha: yup.string().required('O campo senha é obrigatório'),
+});
 
 class UsuarioController {
   async incluir(request, response) {
+    validaSchema.validate(request.body).catch(err => {
+      return response.status(400).json({ error: `${err.errors}` });
+    });
     const usuario = await Usuario.findOne({
       where: {
         email: request.body.email,
