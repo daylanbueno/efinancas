e86bd8f7dd2dee546ed48f42bfef30a18ef98b5c
Dailan Bueno <daylansantos@gmail.com>
2020-06-06 21:37:09 -0300

    Valida campos pessoa, lancamento

--- src/app/controller/LancamentoController.js
+++ src/app/controller/LancamentoController.js
@@ -1,8 +1,19 @@
+import * as yup from 'yup';
 import Lancamento from '../models/Lancamento';
 import Pessoa from '../models/Pessoa';
 
+const validaSchema = yup.object().shape({
+  descricao: yup.string().required('O campo descrição é obrigatório'),
+  categoria: yup.string().required('O campo categoria é obrigatório'),
+  situacao: yup.string().required('O campo situaçao é obrigatório'),
+  valor: yup.number().required('O campo valor é obrigatório'),
+});
+
 class LancamentoController {
   async incluir(req, res) {
+    validaSchema.validate(req.body).catch(err => {
+      return res.status(400).json({ error: err.errors });
+    });
     const lancamento = await Lancamento.create(req.body);
     return res.json(lancamento);
   }
--- src/app/controller/PessoaController.js
+++ src/app/controller/PessoaController.js
@@ -1,7 +1,16 @@
+import * as yup from 'yup';
 import Pessoa from '../models/Pessoa';
+
+const validaSchema = yup.object().shape({
+  nome: yup.string().required('O campo nome é obrigatório'),
+  tipo: yup.string().required('O campo tipo é obrigatório'),
+});
 
 class PessoaController {
   async incluir(request, response) {
+    validaSchema.validate(request.body).catch(err => {
+      return response.status(400).json({ error: err.errors });
+    });
     const pessoa = await Pessoa.create(request.body);
     return response.json(pessoa);
   }
